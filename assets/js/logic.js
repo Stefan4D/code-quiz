8a1f771cbfc36b423e746d917541ec1c5354b5a4
Stefan <blockchain.crypto.stefan@gmail.com>
2023-07-18 13:53:19 +0100

    Added starter filers

---
+++ assets/js/logic.js
@@ -0,0 +1,1 @@
+// This file will contain the application logic for the game including timer, button click event handling, playing associated audio for correct/incorrect and managing the overall game event loop
---
+++ assets/js/questions.js
@@ -0,0 +1,1 @@
+// This file will contain an array of questions including associated multiple choice answers and a flag for which is the correct answer
