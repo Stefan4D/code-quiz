8c36bff540eac95566391de5f7ea97c2e113bae2
Stefan <blockchain.crypto.stefan@gmail.com>
2023-07-18 21:43:34 +0100

    Added setInterval for core timing loop

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,10 +8,40 @@ console.log(questions[0]);
 
 // grab the DOM elements
 const time = document.getElementById("time");
-const questionContainer = document.getElementById("questions"); // to allow to set the style from hide to visible
+const questionContainer = document.getElementById("questions"); // to allow to set the style from hide to visible - programmatically add/remove the class using the .classList.add() and .remove() methods
 const questionTitle = document.getElementById("question-title");
 const choices = document.getElementById("choices");
 
 const startScreen = document.getElementById("start-screen");
 const endScreen = document.getElementById("end-screen");
 const finalScore = document.getElementById("final-score");
+
+// Initialise variables
+let timer = 5;
+
+setInterval(() => {
+  if (timer > 0) {
+    timer -= 1;
+    time.innerText = timer;
+  } else {
+    time.innerText = "Time's up!";
+  }
+}, 1000);
+
+// USER STORY
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+// ACCEPTANCE CRITERIA
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
